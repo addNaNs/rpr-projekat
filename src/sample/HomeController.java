5e0176330a99506e28ae79ac25be919899163d53
addNaNs <adi.saban.1999@gmail.com>
2020-02-07 22:38:42 +0100

    osnovni izgled glavnog prozora za aplikaciju bez ikakve funkcionalnosti

---
+++ src/sample/HomeController.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
+public class HomeController {
+
+    public HomeController() {
+    }
+
+    @FXML
+    public void initialize() {
+    }
+
+
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        HomeController ctrl = new HomeController();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/home.fxml"));
+        loader.setController(ctrl);
+        Parent root = loader.load();
+        primaryStage.setTitle("RegiMaster 2020");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.show();
+        //primaryStage.setResizable(false);
     }
 
 
     public static void main(String[] args) {
         launch(args);
+
     }
 }
