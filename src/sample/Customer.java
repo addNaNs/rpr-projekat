36cf21449c1cfc48755472474b1a1a7edda4ac13
addNaNs <adi.saban.1999@gmail.com>
2020-02-09 00:17:11 +0100

    modifikovao getVehicles u Customer

--- src/sample/Customer.java
+++ src/sample/Customer.java
@@ -1,25 +1,17 @@
 package sample;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Customer extends Person {
 
-    ArrayList<Vehicle> vehicles = new ArrayList<>();
 
     public Customer(int id, String firstName, String lastName, String email, String profileImagePath, Gender sex) {
         super(id, firstName, lastName, email, profileImagePath, sex);
     }
 
-    public Customer(int id, String firstName, String lastName, String email, String profileImagePath, Gender sex, ArrayList<Vehicle> vehicles) {
-        super(id, firstName, lastName, email, profileImagePath, sex);
-        this.vehicles = new ArrayList<>(vehicles);
-    }
-
     public ArrayList<Vehicle> getVehicles() {
-        return vehicles;
-    }
-
-    public void setVehicles(ArrayList<Vehicle> vehicles) {
-        this.vehicles = vehicles;
+        ArrayList<Vehicle> vehicles = RegistrationDAO.getInstance().vehicles();
+        return (ArrayList<Vehicle>) vehicles.stream().filter(v -> v.getOwnerId()==this.getId()).collect(Collectors.toList());
     }
 }
