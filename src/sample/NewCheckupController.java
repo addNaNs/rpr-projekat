77bae7cc631c4fa5b856c419885445802c6c1c3f
addNaNs <adi.saban.1999@gmail.com>
2020-02-12 23:10:31 +0100

    uspjesno biranje bozil u novom pregledu

--- src/sample/NewCheckupController.java
+++ src/sample/NewCheckupController.java
@@ -3,9 +3,16 @@ package sample;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 
 public class NewCheckupController {
@@ -24,13 +31,32 @@ public class NewCheckupController {
     }
 
     public void actionNewVehicle(ActionEvent actionEvent){
-        //dodaj novo TODO
         return;
     }
 
     public void actionOldVehicle(ActionEvent actionEvent){
-        //dodaj novo TODO
-        return;
+        try {
+            Stage stage = new Stage();
+            Parent root = null;
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/singleList.fxml"));
+            VehiclesController vehiclesController = new VehiclesController();
+            loader.setController(vehiclesController);
+            root = loader.load();
+            stage.setTitle("Izaberite vozilo");
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+
+            stage.setOnHiding( event -> {
+                if (vehiclesController.getVehicle() != null) {
+                    vehicle = vehiclesController.getVehicle();
+                    labelSelected.setText("Izabrali ste:" + vehicle);
+                }
+            } );
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void clickCancel(ActionEvent actionEvent){
