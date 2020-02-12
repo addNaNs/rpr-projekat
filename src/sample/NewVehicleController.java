4da5053b3cf66c3df9d51eabea0288f2f347fc82
addNaNs <adi.saban.1999@gmail.com>
2020-02-12 23:27:45 +0100

    dodao fxml i controller za new Vehicle

--- src/sample/NewVehicleController.java
+++ src/sample/NewVehicleController.java
@@ -1,6 +1,5 @@
 package sample;
 
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,49 +7,47 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
+public class NewVehicleController {
 
-public class NewCheckupController {
-    public Label labelSelected;
-    public CheckBox cbBrake, cbSteering, cbLighting, cbEngine, cbElectric;
-
+    public TextField fieldPlates;
     private Vehicle vehicle;
+    private Customer owner;
 
-    public NewCheckupController() {
+    public NewVehicleController() {
         vehicle = null;
     }
 
     @FXML
     public void initialize() {
-        labelSelected.setText("Please select a vehicle");
     }
 
-    public void actionNewVehicle(ActionEvent actionEvent){
+    public void actionNewOwner(ActionEvent actionEvent){
 
     }
 
-    public void actionOldVehicle(ActionEvent actionEvent){
+    public void actionOldOwner(ActionEvent actionEvent){
         try {
             Stage stage = new Stage();
             Parent root = null;
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/singleList.fxml"));
-            VehiclesController vehiclesController = new VehiclesController();
-            loader.setController(vehiclesController);
+            CustomersController customersController = new CustomersController();
+            loader.setController(customersController);
             root = loader.load();
-            stage.setTitle("Izaberite vozilo");
+            stage.setTitle("Izaberite mušteriju");
             stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
             stage.setResizable(false);
             stage.show();
 
             stage.setOnHiding( event -> {
-                if (vehiclesController.getVehicle() != null) {
-                    vehicle = vehiclesController.getVehicle();
-                    labelSelected.setText("Izabrali ste:" + vehicle);
+                if (customersController.getPerson() != null) {
+                    owner = (Customer) customersController.getPerson();
                 }
             } );
 
@@ -61,7 +58,7 @@ public class NewCheckupController {
 
     public void clickCancel(ActionEvent actionEvent){
         vehicle = null;
-        Stage stage = (Stage) labelSelected.getScene().getWindow();
+        Stage stage = (Stage) fieldPlates.getScene().getWindow();
         stage.close();
     }
 
@@ -70,7 +67,7 @@ public class NewCheckupController {
             return;
         }
 
-        Stage stage = (Stage) labelSelected.getScene().getWindow();
+        Stage stage = (Stage) fieldPlates.getScene().getWindow();
         stage.close();
     }
 
